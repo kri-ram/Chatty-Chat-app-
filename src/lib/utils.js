f6504b119ec4760b0b0ed4092f77882dab7b3b91
Ram Krishna <150454428+kri-ram@users.noreply.github.com>
2025-04-24 15:28:39 +0530

    Add files via upload

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,13 +1,7 @@
-import jwt from "jsonwebtoken"
-export const generateToken = (userId,res)=>{
- const token =  jwt.sign({userId},process.env.JWT_SECRET,{
-    expiresIn: "7d"
- })
- res.cookie("jwt" ,token,{
-    maxAge:7*24*60*60*1000,
-    httpOnly:true,// prevent XSS attacks cross-site scripting attacks
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !== "development"
- });
- return token;
-}
+export function formatMessageTime(date) {
+    return new Date(date).toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  }
